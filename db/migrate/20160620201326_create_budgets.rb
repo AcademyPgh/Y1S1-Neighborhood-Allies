995daeb7e2b58c7eb44dbd117eb763c0115f8304
Rob Gorham <robsteeliano@gmail.com>
2016-06-21 02:40:54 +0000

    Took out the budget model and fixed logo model->it's not blob type, it's binary apparently, added notes about the fact we need to have all of this main branch's common functionality done so we can branch without breaking it

--- db/migrate/20160620201326_create_budgets.rb
+++
@@ -1,9 +0,0 @@
-class CreateBudget < ActiveRecord::Migration
-  def change
-    create_table :budget do |t|
-      t.string :budgetrange
-
-      t.timestamps null: false
-    end
-  end
-end
--- db/migrate/20160620202528_create_logos.rb
+++ db/migrate/20160620202528_create_logos.rb
@@ -1,7 +1,7 @@
 class CreateLogos < ActiveRecord::Migration
   def change
     create_table :logos do |t|
-      t.blob :image
+      t.binary :image
 
       t.timestamps null: false
     end
